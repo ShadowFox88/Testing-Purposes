c0e644c1b8738f533ff30d0c07e001ab1723cfd2
cyrus01337 <cyrus21337@gmail.com>
2022-05-15 11:33:00 +0100

    Remove inactive product tests

--- src/ServerScriptService/Tests/Logging.server.lua
+++
@@ -1,9 +0,0 @@
-local RS = game:GetService("ReplicatedStorage")
-
-local Logging = require(RS.Logging)
-      Logging.URL = "https://logging-omega.vercel.app/"
-
-local WEBHOOK = "https://canary.discord.com/api/webhooks/921051613001556008/iKu6J01ij35PbUP64QQCc9-Km57P1tYaoZVARBgS0y-AuALQa8ThHUDbHMjgL0Izm2qG"
-local secret = Logging.authenticate(WEBHOOK)
-
-print(secret)
