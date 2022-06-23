804d86d1efa59983a6ea3c979b6c4c20a407ba86
cyrus01337 <cyrus21337@gmail.com>
2022-06-23 03:53:31 +0100

    Remove utils backup

--- src/Utils/Types.lua
+++
@@ -1,9 +0,0 @@
-local Types = {}
-
-export type Array<V> = { [number]: V }
-export type Dictionary<V> = { [string]: V }
-export type Function<P..., R...> = (P...) -> R...
-export type Mapping<K, V> = { [K]: V }
-export type Table = Array<any> | Mapping<any, any>
-
-return Types
