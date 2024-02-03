e41efce476135ef10006f3bd8f9d0c5fa05a8f4d
cyrus01337 <cyrus21337@gmail.com>
2024-02-03 10:15:15 +0000

    Cleanup example and disable linting

--- src/ServerStorage/References/FunctionParameterisation.server.lua
+++ src/ServerStorage/References/FunctionParameterisation.server.lua
@@ -1,25 +1,27 @@
+--!nolint
 --!strict
 local function closure(x: number, y: number): () -> number
-    return function()
-        return x + y
-    end
+	return function()
+		return x + y
+	end
 end
-
 
 local callback: () -> number = closure(2, 2)
+
 local value: number = callback()
+
 print(value)
 
-
 local function HOF(callback: () -> any): () -> any
-    return function()
-        return callback()
-    end
+	return function()
+		return callback()
+	end
 end
 
-
 local callback: () -> any = HOF(function()
-    return "Hello, World!"
+	return "Hello, World!"
 end)
+
 local value: any = callback()
+
 print(value)
