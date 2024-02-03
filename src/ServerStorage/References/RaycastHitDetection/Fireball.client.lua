f37a42c6bc80ba68966d7bfc1bf427750c63fd1a
cyrus01337 <cyrus21337@gmail.com>
2024-02-03 08:23:14 +0000

    Refactor

--- src/ServerStorage/References/RaycastHitDetection/Fireball.client.lua
+++ src/ServerStorage/References/RaycastHitDetection/Fireball.client.lua
@@ -1,24 +1,38 @@
-local RS = game:GetService("ReplicatedStorage")
+local ContextActionService = game:GetService("ContextActionService")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Utils = require(RS.Utils)
-local _S = Utils.Services
-
-local player = _S.Players.LocalPlayer
-local playerChar = player.Character or player.CharacterAdded:Wait()
-local playerHead = playerChar:WaitForChild("Head", 60)
+local player = Players.LocalPlayer
 local playerMouse = player:GetMouse()
-      playerMouse.TargetFilter = workspace:WaitForChild("Projectiles", 60)
-local Fireball = Utils.waitForDescendant(RS, "Fireball")
+playerMouse.TargetFilter = workspace:WaitForChild("Projectiles", 60)
+local Fireball: RemoteEvent = ReplicatedStorage:WaitForChild("Fireball")
 
+-- defined here because lazy
+local function debounce(callback)
+	local running = false
 
-local function onCastMagic(action, state, input)
-    if state ~= Enum.UserInputState.Begin then return end
+	return function(...)
+		if running then
+			return
+		end
 
-    if action == "Fireball" then
-        Fireball:FireServer(playerMouse.Hit.p)
-        --task.wait(5)
-    end
+		running = true
+		local result = callback(...)
+		running = false
+
+		return result
+	end
 end
 
+local function onCastMagic(action, state, input)
+	if state ~= Enum.UserInputState.Begin then
+		return
+	end
 
-_S.ContextActionService:BindAction("Fireball", Utils.debounce(onCastMagic), false, Enum.KeyCode.F)
+	if action == "Fireball" then
+		Fireball:FireServer(playerMouse.Hit.p)
+		--task.wait(5)
+	end
+end
+
+ContextActionService:BindAction("Fireball", debounce(onCastMagic), false, Enum.KeyCode.F)
